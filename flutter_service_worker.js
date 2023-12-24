ed5429e31efa3681920458b377d8ca1d20d47084
djh <jydwww@naver.com>
2023-12-24 14:33:47 +0900

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "c7899c5aa808cfb42a5ddaba0c26c1ac",
-"index.html": "a6ae2f80d04dc73ef90fde2884736168",
-"/": "a6ae2f80d04dc73ef90fde2884736168",
-"main.dart.js": "98583facd9be996895ee6517ed186e2d",
+"index.html": "a32adc50ac595c4918790763b8b0d587",
+"/": "a32adc50ac595c4918790763b8b0d587",
+"main.dart.js": "b9682fc8f29e5add03a635576eeb2a30",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
